c228d163915fdf2ac12130e2ad880106393891d6
fartun <fartunwario34@gmail.com>
2023-03-19 22:09:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const newHeaderText = document.createTextNode("Ayman Khalif is the champion");
+newHeader.appendChild(newHeaderText);
